abfcb30f7b79a2895f04e731f8ef5d9061279f65
Dimitris Kagialis <kagialisd@gmail.com>
2024-05-21 17:05:24 +0300

    Refactor certificate setup for multi-domain services
    This update adapts the SSL certifications setup for multiple services with different domains. Changes were made to the Dockerfile, nginx configuration, and shell scripts to handle these services individually. This ensures that each service has its unique SSL certificate while maintaining its domain settings.

--- cert/cert-entrypoint.sh
+++ cert/cert-entrypoint.sh
@@ -1,30 +1,57 @@
 #!/bin/sh
 
-export DOMAIN=${DOMAIN}
 export EMAIL=${EMAIL}
 
+export DC_DTM_SERVICE_DOMAIN=${DC_DTM_SERVICE_DOMAIN}
+
 # Ensure we have a folder for the certificates
-if [ ! -d /usr/share/nginx/certificates ]; then
-    echo "Creating certificate folder"
-    mkdir -p /usr/share/nginx/certificates
+if [ ! -d /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN ]; then
+    echo "Creating DC_DTM_SERVICE_DOMAIN certificate folder"
+    mkdir -p /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN
 fi
 
 ### If certificates do not exist yet, create self-signed ones before we start nginx
-if [ ! -f /usr/share/nginx/certificates/fullchain.pem ]; then
-    echo "Generating self-signed certificate"
-    openssl genrsa -out /usr/share/nginx/certificates/privkey.pem 4096
-    openssl req -new -key /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/cert.csr -nodes -subj \
-    "/C=PT/ST=World/L=World/O=$DOMAIN/OU=egi lda/CN=$DOMAIN"
-    openssl x509 -req -days 365 -in /usr/share/nginx/certificates/cert.csr -signkey /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/fullchain.pem
+if [ ! -f /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN/fullchain.pem ]; then
+    echo "Generating DC_DTM_SERVICE_DOMAIN self-signed certificate"
+    openssl genrsa -out /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN/privkey.pem 4096
+    openssl req -new -key /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN/privkey.pem -out /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN/cert.csr -nodes -subj \
+    "/C=PT/ST=World/L=World/O=$DC_DTM_SERVICE_DOMAIN/OU=egi lda/CN=$DC_DTM_SERVICE_DOMAIN"
+    openssl x509 -req -days 365 -in /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN/cert.csr -signkey /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN/privkey.pem -out /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN/fullchain.pem
 fi
 
-if [ -n "$DOMAIN" ] && [ "$DOMAIN" != "localhost" ]; then
+if [ -n "$DC_DTM_SERVICE_DOMAIN" ] && [ "$DC_DTM_SERVICE_DOMAIN" != "localhost" ]; then
     ### Send certbot emission/renewal to background
     $(while :; do /opt/request.sh; sleep "${RENEW_INTERVAL:-12h}"; done;) &
 
     ### Check for changes in the certificate (i.e renewals or first start) in the background
-    $(while inotifywait -e close_write /usr/share/nginx/certificates; do echo "Reloading nginx with new certificate"; nginx -s reload; done) &
+    $(while inotifywait -e close_write /usr/share/nginx/certificates/DC_DTM_SERVICE_DOMAIN; do echo "Reloading nginx with DC_DTM_SERVICE_DOMAIN new certificate"; nginx -s reload; done) &
 fi
+
+export WF_DTM_SERVICE_DOMAIN=${WF_DTM_SERVICE_DOMAIN}
+
+# Ensure we have a folder for the certificates
+if [ ! -d /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN ]; then
+    echo "Creating WF_DTM_SERVICE_DOMAIN certificate folder"
+    mkdir -p /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN
+fi
+
+### If certificates do not exist yet, create self-signed ones before we start nginx
+if [ ! -f /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN/fullchain.pem ]; then
+    echo "Generating WF_DTM_SERVICE_DOMAIN self-signed certificate"
+    openssl genrsa -out /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN/privkey.pem 4096
+    openssl req -new -key /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN/privkey.pem -out /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN/cert.csr -nodes -subj \
+    "/C=PT/ST=World/L=World/O=$WF_DTM_SERVICE_DOMAIN/OU=egi lda/CN=$WF_DTM_SERVICE_DOMAIN"
+    openssl x509 -req -days 365 -in /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN/cert.csr -signkey /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN/privkey.pem -out /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN/fullchain.pem
+fi
+
+if [ -n "$WF_DTM_SERVICE_DOMAIN" ] && [ "$WF_DTM_SERVICE_DOMAIN" != "localhost" ]; then
+    ### Send certbot emission/renewal to background
+    $(while :; do /opt/request.sh; sleep "${RENEW_INTERVAL:-12h}"; done;) &
+
+    ### Check for changes in the certificate (i.e renewals or first start) in the background
+    $(while inotifywait -e close_write /usr/share/nginx/certificates/WF_DTM_SERVICE_DOMAIN; do echo "Reloading nginx with WF_DTM_SERVICE_DOMAIN new certificate"; nginx -s reload; done) &
+fi
+
 
 ### Start nginx with daemon off as our main pid
 echo "Starting nginx"
--- cert/cert-request.sh
+++ cert/cert-request.sh
@@ -4,12 +4,12 @@ if [ ! -f /var/www/html ]; then
     mkdir -p /var/www/html
 fi
  
-if [ -n "$DOMAIN" ] && [ "$DOMAIN" != "localhost" ]; then
+if [ -n "$DC_DTM_SERVICE_DOMAIN" ] && [ "$DC_DTM_SERVICE_DOMAIN" != "localhost" ]; then
 	certbot certonly \
 			--config-dir ${LETSENCRYPT_DIR:-/etc/letsencrypt} \
 			--dry-run \
 			--agree-tos \
-			--domains "$DOMAIN" \
+			--DC_DTM_SERVICE_DOMAINs "$DC_DTM_SERVICE_DOMAIN" \
 			--email $EMAIL \
 			--expand \
 			--noninteractive \
@@ -17,13 +17,37 @@ if [ -n "$DOMAIN" ] && [ "$DOMAIN" != "localhost" ]; then
 			--webroot-path /var/www/html \
 			$OPTIONS || true
 
-	if [ -f ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem ]; then
+	if [ -f ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DC_DTM_SERVICE_DOMAIN/privkey.pem ]; then
 		chmod +rx ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live
 		chmod +rx ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive
-		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DOMAIN}/fullchain*.pem
-		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DOMAIN}/privkey*.pem
-		cp ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem /usr/share/nginx/certificates/privkey.pem
-		cp ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem /usr/share/nginx/certificates/fullchain.pem
+		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DC_DTM_SERVICE_DOMAIN}/fullchain*.pem
+		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DC_DTM_SERVICE_DOMAIN}/privkey*.pem
+		cp ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DC_DTM_SERVICE_DOMAIN/privkey.pem /usr/share/nginx/certificates/$DC_DTM_SERVICE_DOMAIN/privkey.pem
+		cp ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DC_DTM_SERVICE_DOMAIN/fullchain.pem /usr/share/nginx/certificates/$DC_DTM_SERVICE_DOMAIN/fullchain.pem
+		echo "Copied new certificate to /usr/share/nginx/certificates"
+	fi
+fi
+
+if [ -n "$WF_DTM_SERVICE_DOMAIN" ] && [ "$WF_DTM_SERVICE_DOMAIN" != "localhost" ]; then
+	certbot certonly \
+			--config-dir ${LETSENCRYPT_DIR:-/etc/letsencrypt} \
+			--dry-run \
+			--agree-tos \
+			--WF_DTM_SERVICE_DOMAINs "$WF_DTM_SERVICE_DOMAIN" \
+			--email $EMAIL \
+			--expand \
+			--noninteractive \
+			--webroot \
+			--webroot-path /var/www/html \
+			$OPTIONS || true
+
+	if [ -f ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$WF_DTM_SERVICE_DOMAIN/privkey.pem ]; then
+		chmod +rx ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live
+		chmod +rx ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive
+		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${WF_DTM_SERVICE_DOMAIN}/fullchain*.pem
+		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${WF_DTM_SERVICE_DOMAIN}/privkey*.pem
+		cp ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$WF_DTM_SERVICE_DOMAIN/privkey.pem /usr/share/nginx/certificates/$WF_DTM_SERVICE_DOMAIN/privkey.pem
+		cp ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$WF_DTM_SERVICE_DOMAIN/fullchain.pem /usr/share/nginx/certificates/$WF_DTM_SERVICE_DOMAIN/fullchain.pem
 		echo "Copied new certificate to /usr/share/nginx/certificates"
 	fi
 fi
